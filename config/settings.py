88c147d9171d440599536d06cfd96c1c31e3e242
Elias Saffarmoghadam <esaffarmoghadam@gmail.com>
2026-03-12 11:31:37 +0100

    stage all changes and run py manage.py migrate

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'traffic',
+    'vehicles',
+    'alerts',
+    'cameras',
+    'users',
 ]
 
 MIDDLEWARE = [
